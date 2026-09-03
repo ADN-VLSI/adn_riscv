b260fca6abaa5637e2d68f0b92d5dc25f6e94b0f
Shykul Islam Siam <shykulislam32@gmail.com>
2026-09-03 13:01:31 +0600

    modified:   local.f modified:   regression_list

--- local.f
+++ local.f
@@ -1,6 +1,5 @@
 -i ${ADN_RISCV}/testbench
 ${ADN_RISCV}/testbench/adn_riscv_reg_file_tb.sv
-${ADN_RISCV}/testbench/dummy_tb.sv
 ${ADN_RISCV}/testbench/adn_riscv_instr_decoder_tb.sv
 ${ADN_RISCV}/testbench/adn_riscv_instr_order_checker_tb.sv
 ${ADN_RISCV}/testbench/adn_riscv_btb_tb.sv
